4e9678b9161c16943eb90b34b1207659962cc73e
dudushy <eduardotalarico1337@gmail.com>
2023-04-07 20:14:36 -0300

    Chat working

--- src/main.py
+++ src/main.py
@@ -2,18 +2,31 @@
 #* -- Imports
 import os
 from dotenv import load_dotenv
+import openai
 
 #* -- Variables
 path = os.path.dirname(__file__) #? Directory path
 
 load_dotenv(dotenv_path=".env") #? Load .env file
 
+openai.organization = os.getenv("ORG_ID") #? Set OpenAI organization ID
+openai.api_key = os.getenv("API_KEY") #? Set OpenAI API key
+
 #* -- Functions
 def clearConsole() -> None: #? Clear console
     os.system("cls" if os.name == "nt" else "clear")
 
 def main() -> None:
-    print(os.getenv("API_KEY"))
+    # print(openai.Model.list())
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{
+            "role": "user",
+            "content": "Hello!"
+        }]
+    )
+
+    print(completion.choices[0].message.content)
 
 #! Main
 if __name__ == "__main__":
