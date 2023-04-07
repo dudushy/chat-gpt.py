6fc3f587bf4dcdd24c38134a10d9fdf8673ee236
dudushy <eduardotalarico1337@gmail.com>
2023-04-07 19:48:13 -0300

    .env working

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 # pip freeze > requirements.txt
 #* -- Imports
 import os
+from dotenv import load_dotenv
 
 #* -- Variables
 path = os.path.dirname(__file__) #? Directory path
+
+load_dotenv(dotenv_path=".env") #? Load .env file
 
 #* -- Functions
 def clearConsole() -> None: #? Clear console
     os.system("cls" if os.name == "nt" else "clear")
 
 def main() -> None:
-    pass
+    print(os.getenv("API_KEY"))
 
 #! Main
 if __name__ == "__main__":
